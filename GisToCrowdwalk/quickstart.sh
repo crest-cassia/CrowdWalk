d188845e31299551a87e4acc729cb0b2934148f6
Mitsuo Saito <mitsuo-saitou@aist.go.jp>
2014-06-17 12:01:48 +0900

    GisToCrowdwalk を JDK 1.7 以降でもビルド可能にし、また Cygwin 上でも動作するようにした。

--- GisToCrowdwalk/quickstart.sh
+++ GisToCrowdwalk/quickstart.sh
@@ -2,6 +2,11 @@
 
 CP=build/libs/GisToCrowdwalk.jar:.:gis/geotools-2.7-M3/gt-shapefile-2.7-M3.jar:gis/geotools-2.7-M3/gt-main-2.7-M3.jar:gis/geotools-2.7-M3/gt-swing-2.7-M3.jar:lib/jruby.jar
 #CP=build/libs/ImportFromGIS.jar:.
+OS=`uname -a`
+case "$OS" in
+    *"CYGWIN"*)
+        CP=build/libs/GisToCrowdwalk.jar\;.\;gis/geotools-2.7-M3/gt-shapefile-2.7-M3.jar\;gis/geotools-2.7-M3/gt-main-2.7-M3.jar\;gis/geotools-2.7-M3/gt-swing-2.7-M3.jar\;lib/jruby.jar
+        ;;
+esac
 echo "java -cp $CP nodagumi.ananPJ.Editor.ImportGis"
-java -Xms1024M -Xmx1024M -cp $CP nodagumi.ananPJ.Editor.ImportGis
-
+java -Dfile.encoding=UTF-8 -cp $CP nodagumi.ananPJ.Editor.ImportGis
