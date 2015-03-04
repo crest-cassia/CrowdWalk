ea9f4adcd2245273aaed16b36dbae56cbfba4e83
Mitsuo Saito <mitsuo-saitou@aist.go.jp>
2015-03-04 11:58:04 +0900

    実行した quickstart.sh がカレントディレクトリに存在する場合は、環境変数 CROWDWALK の設定に関わらずカレントディレクトリの CrowdWalk を起動する様にした。
    ・不要なファイルも削除。

--- netmas/quickstart.sh
+++ netmas/quickstart.sh
@@ -5,32 +5,39 @@ if test "$CROWDWALK" = "" ; then
 	CROWDWALK='.'
 fi
 
+# カレントディレクトリの CrowdWalk を優先
+if test "$(dirname $0)" = "." ; then
+	DIR='.'
+else
+        DIR=CROWDWALK
+fi
+
 JAVA='java'
 JAVAOPT='-Dfile.encoding=UTF-8'
-DYLD=$CROWDWALK/libs
-JAR=$CROWDWALK/build/libs/netmas.jar
+DYLD=$DIR/libs
+JAR=$DIR/build/libs/netmas.jar
 
 OS=`uname -a`
 case "$OS" in
     *"Darwin"*)
         echo " > Load Mac OS X libraries..."
-        DYLD=$CROWDWALK/libs/macosx
+        DYLD=$DIR/libs/macosx
         ;;
     *"CYGWIN"*"64"*)
         echo " > Load Windows amd64 libraries..."
-        DYLD=$CROWDWALK/libs/windows/amd64
+        DYLD=$DIR/libs/windows/amd64
         ;;
     *"CYGWIN"*"i686"*)
         echo " > Load Windows i386 libraries..."
-        DYLD=$CROWDWALK/libs/windows/i386
+        DYLD=$DIR/libs/windows/i386
         ;;
     *"Linux"*"x86_64"*)
         echo " > Load linux amd64 libraries..."
-        DYLD=$CROWDWALK/libs/linux/amd64
+        DYLD=$DIR/libs/linux/amd64
         ;;
     *"Linux"*"x86_32"*)
         echo " > Load linux i386 libraries..."
-        DYLD=$CROWDWALK/libs/linux/i386
+        DYLD=$DIR/libs/linux/i386
         ;;
     *)
         echo " > Current OS may not be supported..."
