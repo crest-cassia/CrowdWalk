e786921c7d286562f0dde00fe4e8824d93c3d57f
JunpeiTsuji <tsuji@dhcp151047.a01.aist.go.jp>
2015-04-18 01:51:07 +0900

    scenarioMakerによって出力されたデータファイルをチェックして, 実行時関等の統計データを出力するツールを追加

---
+++ netmas/tools/tsjTools/aggregateAllSimulationSteps.rb
@@ -0,0 +1,52 @@
+require 'json'
+
+# デプロイしたシミュレーションデータの結果をソートして並べるユーティリティースクリプト
+# 
+
+
+# デプロイ先ターゲットディレクトリのパス
+target_dir_path = "/mnt/exhdd/data/"
+
+
+# シミュレーションデータを格納する配列
+stats = Array.new
+
+
+# ディレクトリのタイトルを配列に入れる
+keys = Dir::entries( target_dir_path )
+
+# カレントディレクトリと親ディレクトリのパスは除く
+keys.delete(".")
+keys.delete("..")
+
+
+# すべてのシミュレーションデータの steps と query を配列に格納する
+keys.each do |key|
+	log_json_path = target_dir_path + key + "/log.json"
+	
+	log_json_data = File.open( log_json_path ) do |io|
+		JSON.load(io)
+	end
+
+	steps_string = log_json_data['steps']
+	steps_integer = steps_string.to_i
+	
+	stats.push({ 'steps'=>steps_integer, 'query'=>key })
+
+end
+	
+
+
+# steps で sort
+sorted_stats = stats.sort do |a, b|
+	a['steps'] <=> b['steps']
+end
+
+# ソート済みの配列を表示
+sorted_stats.each do |stat|
+	puts stat
+end
+
+# データの数を表示
+puts "number of files: #{sorted_stats.size}" 
+
