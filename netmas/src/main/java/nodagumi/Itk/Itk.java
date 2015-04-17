d6e92eaa083a66fcb5c242203d484c36778a282d
Itsuki Noda <i.noda@aist.go.jp>
2015-04-17 23:01:42 +0900

    fallback に関する優先順位などのドキュメントを記載。

--- netmas/src/main/java/nodagumi/Itk/Itk.java
+++ netmas/src/main/java/nodagumi/Itk/Itk.java
@@ -45,7 +45,39 @@ import net.arnx.jsonic.JSON ;
  *  <li> {@link nodagumi.ananPJ.misc.AgentGenerationFile エージェント生成ルール}</li>
  *  <li> {@link nodagumi.ananPJ.misc.GenerateAgent エージェント設定 config}</li>
  *  <li> {@link nodagumi.ananPJ.Agents.Think.ThinkEngine エージェント行動ルール}</li>
+ *  <li> {@link #fallback 各種設定とFallback}</li>
  * </ul>
+ * <h2><a name="fallback">各種設定とFallback</a></h2>
+ * Agent のconfig など、シミュレーションの各種設定は、以下の優先順位で決定される。
+ * <ol>
+ *   <li> Agentに関するパラメータについては、
+ *       {@link nodagumi.ananPJ.misc.AgentGenerationFile エージェント生成ルール}
+ *       の中の、"agentType" の項目で、エージェントクラス名と共に規定値を
+ *       指定できる。
+ *       設定できる項目はエージェントクラスにより異なる。
+ *       各クラスでの設定項目は、
+ *       {@link nodagumi.ananPJ.misc.GenerateAgent エージェント設定 config} を
+ *       参照。
+ *   </li>
+ *   <li> 全ての設定項目について、
+ *        シミュレーション実行のための properties ファイルの
+ *        "fallback_file" で指定した JSON ファイルにおいて規定値を指定できる。
+ *        記載例は、"sample/simpleGrid/fallbackParameters.json" を参照。
+ *   </li>
+ *   <li> 全ての設定項目について、
+ *        開発レポジトリの "src/main/resources/fallbackParameters.json" に
+ *        おいて規定値を指定できる。
+ *        このファイルは、buildする段階で jar ファイルに取り込まれる。
+ *   </li>
+ *   <li> 上記のいずれにも指定されなかった設定項目については、プログラム中で、
+ *        指定した定数が規定値として指定される。
+ *   </li>
+ * </ol>
+ * 上記の優先順位に則ったパラメータの設定は、できるだけ、標準的なメソッドとして
+ * 用意されるべきである。
+ * 例えば、{@link nodagumi.ananPJ.Agent.AgentBase AgentBase クラス}の
+ * getDoubleFromConfig(), getIntFromConfig(), getTermFromConfig() などの
+ * メソッドである。
  */
 public class Itk {
     //------------------------------------------------------------
