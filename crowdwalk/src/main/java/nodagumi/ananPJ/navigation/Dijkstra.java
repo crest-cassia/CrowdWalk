4ebfa7350516ec48beb02a8b79bc695708d7bf69
Itsuki Noda <i.noda@aist.go.jp>
2016-01-29 03:22:40 +0900

    Dijkstra.calc の無駄を省いてみた。

--- crowdwalk/src/main/java/nodagumi/ananPJ/navigation/Dijkstra.java
+++ crowdwalk/src/main/java/nodagumi/ananPJ/navigation/Dijkstra.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.LinkedHashMap;
+import java.util.ArrayList;
 
 import nodagumi.ananPJ.NetworkMap.Link.MapLink;
 import nodagumi.ananPJ.NetworkMap.Node.MapNode;
@@ -14,33 +15,52 @@ import nodagumi.ananPJ.navigation.CalcPath.PathChooser;
 
 import nodagumi.Itk.Itk;
 
+//======================================================================
+/**
+ * Dijkstra法で最短経路探索。
+ */
 public class Dijkstra {
+    //============================================================
+    /**
+     * 探索結果の格納用クラス。
+     */
     static public class Result extends LinkedHashMap<MapNode, NodeLinkLen> {}
 
-    static public Result calc(MapNodeTable subgoals,
-            PathChooser chooser) {
+    //============================================================
+    //------------------------------------------------------------
+    /**
+     * 探索メインルーチン。(obsolete)
+     * @param subgoals 目標とするゴール集合。
+     * @param chooser パスを選ぶ際の距離の調整ツール。
+     * @return 探索結果。Resule class のインスタンス。
+     */
+    static public Result calc0(MapNodeTable subgoals,
+                              PathChooser chooser) {
         Result frontier = new Result();
 
+        // ゴールノードは、initial cost で。
         int count = 0;
         for (MapNode subgoal : subgoals) {
             double cost = chooser.initialCost(subgoal);
             frontier.put(subgoal, new NodeLinkLen(null, null, cost));
-            //System.err.print(subgoal + ".");
             ++count;
         }
-        //System.err.println(count + "subgoals");
 
+        // 探索ループ。
         while (true) {
             double minLength = Double.POSITIVE_INFINITY;
             MapNode bestNode = null;
             MapNode pred = null;
             MapLink bestNext = null;
             for (MapNode frontierNode : frontier.keySet()) {
-                for (MapLink nextLink : frontierNode.getValidReverseLinkTable()) {
+                for (MapLink nextLink :
+                         frontierNode.getValidReverseLinkTable()) {
                     MapNode other_node = nextLink.getOther(frontierNode);
                     if (frontier.containsKey(other_node)) continue;
-                    double len = frontier.get(frontierNode).len + 
-                        nextLink.getLength() * chooser.evacuationRouteCost(nextLink);
+                    double len =
+                        frontier.get(frontierNode).len
+                        + (nextLink.getLength()
+                           * chooser.evacuationRouteCost(nextLink));
                     if (len < minLength) {
                         minLength = len;
                         bestNode =  other_node;
@@ -53,13 +73,82 @@ public class Dijkstra {
                 break;
             }
             frontier.put(bestNode, new NodeLinkLen(pred, bestNext, minLength));
-            // System.err.println("Dijkstra new frontier: " +
-                        // bestNode.getTags());
-            // System.err.print(".");
             ++count;
         }
-        //System.err.print(count);
         return frontier;
+    }
+
+    //============================================================
+    //------------------------------------------------------------
+    /**
+     * 探索メインルーチン。by I.Noda
+     * 上記の calc は、無駄が多いと思われる。何度も同じノードを展開している。
+     * 下記はそれを避ける。
+     * @param subgoals 目標とするゴール集合。
+     * @param chooser パスを選ぶ際の距離の調整ツール。
+     * @return 探索結果。Resule class のインスタンス。
+     */
+    static public Result calc(MapNodeTable subgoals,
+                              PathChooser chooser) {
+        Result frontier = new Result();
+        ArrayList<MapNode> closedList = new ArrayList<MapNode>() ;
+        Result result = new Result() ;
+
+        // ゴールノードは、initial cost で。
+        int count = 0;
+        for (MapNode subgoal : subgoals) {
+            double cost = chooser.initialCost(subgoal);
+            NodeLinkLen nll = new NodeLinkLen(null, null, cost) ;
+            frontier.put(subgoal, nll) ;
+            result.put(subgoal, nll) ;
+            ++count;
+        }
+
+        // 探索ループ。
+        while (true) {
+            double minLength = Double.POSITIVE_INFINITY;
+            MapNode bestNode = null;
+            MapNode pred = null;
+            MapLink bestNext = null;
+            closedList.clear() ;
+            for (MapNode frontierNode : frontier.keySet()) {
+                int countPerNode = 0 ;
+                for (MapLink nextLink :
+                         frontierNode.getValidReverseLinkTable()) {
+                    MapNode other_node = nextLink.getOther(frontierNode);
+                    if (result.containsKey(other_node)) continue;
+                    countPerNode ++ ;
+                    double len =
+                        result.get(frontierNode).len
+                        + (nextLink.getLength()
+                           * chooser.evacuationRouteCost(nextLink));
+                    if (len < minLength) {
+                        minLength = len;
+                        bestNode =  other_node;
+                        bestNext = nextLink;
+                        pred = frontierNode;
+                    }
+                }
+                // すべてのリンク先がすでに探査済みであれば、そのノードは close
+                if(countPerNode == 0) {
+                    closedList.add(frontierNode) ;
+                }
+            }
+            // close されたノードを frontiner から除去。
+            for(MapNode node : closedList) {
+                frontier.remove(node) ;
+            }
+            // 新たに付け加えるものがなければ、探査終わり。
+            if (null == bestNode) {
+                break;
+            }
+            // 新しノードを frontier に追加。
+            NodeLinkLen nll = new NodeLinkLen(pred, bestNext, minLength) ;
+            frontier.put(bestNode, nll) ;
+            result.put(bestNode, nll) ;
+            ++count;
+        }
+        return result ;
     }
 
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
