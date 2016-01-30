2eae3c8019130d105fb08c4b32f9b0c304676ffa
Itsuki Noda <i.noda@aist.go.jp>
2016-01-30 15:46:14 +0900

    NavigationHint の final をはずす。

--- crowdwalk/src/main/java/nodagumi/ananPJ/navigation/NavigationHint.java
+++ crowdwalk/src/main/java/nodagumi/ananPJ/navigation/NavigationHint.java
@@ -9,17 +9,35 @@ import java.lang.ClassNotFoundException;
 import nodagumi.ananPJ.NetworkMap.Link.MapLink;
 import nodagumi.ananPJ.NetworkMap.Node.MapNode;
 
+//======================================================================
+/**
+ * 最適ルーティングの情報。
+ */
 public class NavigationHint implements Comparable<NavigationHint> {
-    final public MapNode exit;
-    final public MapLink way;
-    final public double distance;
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    /** 最終目標ゴールタグ */
+    /** 起点ノード */
+    /** 向かう方向のノード */
+    public MapNode exit;
+    /** 辿るリンク */
+    public MapLink way;
+    /** ゴールまでの距離 */
+    public double distance;
 
-    public NavigationHint(final MapNode e, final MapLink w, final double d) {
+    //--------------------------------------------------
+    /**
+     * constructor.
+     */
+    public NavigationHint(MapNode e, MapLink w, double d) {
         exit = e;
         way = w;
         distance = d;
     }
 
+    //--------------------------------------------------
+    /**
+     * compare.
+     */
     public int compareTo(NavigationHint e) {
         if (e.distance < distance) return -1;
         else if (e.distance > distance) return 1;
