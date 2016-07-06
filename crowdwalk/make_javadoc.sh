413c3f1b5ac82c1ce52f5b828ba40e2e8de607cc
Mitsuo Saito <mitsuo-saitou@aist.go.jp>
2016-07-06 17:01:10 +0900

    JavadocファイルをGitの管理対象から外した。
    最新commitに更新した後、最初にgradleを実行した時だけJavadocを作り直す様にした。

--- crowdwalk/make_javadoc.sh
+++ crowdwalk/make_javadoc.sh
@@ -1,6 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+
+# quickstart.sh のタイムスタンプを commit された日時にする
+TIME=`git log --pretty=format:%ci -n1`
+STAMP=`date -d "$TIME" +"%y%m%d%H%M.%S"`
+touch -t $STAMP quickstart.sh
+
+JAVADOC_FILE="doc/javadoc/nodagumi/ananPJ/package-summary.html"
+
+if [[ -f $JAVADOC_FILE ]]; then
+    if [[ $JAVADOC_FILE -nt quickstart.sh ]]; then
+        echo "Javadoc is up to date."
+        exit 0
+    fi
+fi
 
 "$JAVA_HOME/bin/javadoc" -d ./doc/javadoc -sourcepath ./src/main/java -encoding utf-8 -charset UTF-8 -subpackages nodagumi -notimestamp -windowtitle "crowdwalk API"
 
-# gradle が FAILURE exception を発生するのを回避させるため
+# gradle が FAILURE exception を出してしまうのを回避する
 exit 0
