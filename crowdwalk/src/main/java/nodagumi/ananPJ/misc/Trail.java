fabfa2884b05f3d6c13b00a66549eaa8c8d18d3f
Itsuki Noda <i.noda@aist.go.jp>
2017-06-20 20:25:55 +0900

    add Trail class. （道半ば）

---
+++ crowdwalk/src/main/java/nodagumi/ananPJ/misc/Trail.java
@@ -0,0 +1,124 @@
+// -*- mode: java; indent-tabs-mode: nil -*-
+/** Trail class
+ * @author:: Itsuki Noda
+ * @version:: 0.0 2017/06/20 I.Noda
+ * <B>History:</B>
+ * <UL>
+ *   <LI> [2017/06/20]: Create This File. </LI>
+ *   <LI> [YYYY/MM/DD]: add more </LI>
+ * </UL>
+ * <B>Usage:</B>
+ * ...
+ */
+
+package nodagumi.ananPJ.misc ;
+
+import java.util.ArrayList;
+import nodagumi.Itk.*;
+
+//======================================================================
+/**
+ * Trail class.
+ * store trail (history) information for log output.
+ * used in AgentTrailLog. 
+ */
+public class Trail {
+    //============================================================
+    /**
+     *  Content of Trail.
+     *  should have method getJsonObject().
+     */
+    static public interface Content {
+	//----------------------------------------
+	/**
+	 *  return a Number, a String, a Map, a List, or 
+	 *  an Object that can be translated to JSON.
+	 *  In the case of Number or String, it will be translated to the value.
+	 *  In the case of a List, it will be translated to a JSON array.
+	 *  In the case of a Map, it will be traslated to a JSON object.
+	 *  In the case of an Object, it will be traslated to a JSON object,
+	 *  which consists of pair of name and value of public field.
+	 */
+	public Object getJsonObject() ;
+    } // interface Content
+    
+    //============================================================
+    /**
+     *  One record of Trail.
+     */
+    static public class Record {
+	//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+	/**
+	 * timestamp
+	 */
+	public SimTime timestamp ;
+	
+	//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+	/**
+	 * content of the record.
+	 */
+	public Content content ;
+	
+	//----------------------------------------
+	/**
+	 *  initialize.
+	 */
+	public Record(SimTime _timestamp, Content _content) {
+	    timestamp = _timestamp ;
+	    content = _content ;
+	}
+	    
+	//----------------------------------------
+	/**
+	 *  get json object.
+	 */
+	public Object getJsonObject() {
+	    ArrayList<Object> jsonObject = new ArrayList<Object>(2) ;
+	    
+	    jsonObject.add(new Integer((int)timestamp.getRelativeTime())) ;
+	    jsonObject.add(content.getJsonObject()) ;
+	    
+	    return jsonObject ;
+	}
+    } // class Record
+    
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    /**
+     *  Array of Trail Record
+     */
+    public ArrayList<Record> recordList = new ArrayList<Record>() ;
+
+    //------------------------------------------------------------
+    /**
+     * initialization.
+     */
+    public Trail(){ } ;
+
+    //------------------------------------------------------------
+    /**
+     * get JsonObject, which consists of a list of JsonObject of each item.
+     */
+    public ArrayList<Object> getJsonObject() {
+        ArrayList<Object> jsonObject = new ArrayList<Object>() ;
+
+	for(Record record : recordList) {
+	    jsonObject.add(record.getJsonObject()) ;
+	}
+
+	return jsonObject ;
+    }
+
+    //------------------------------------------------------------
+    /**
+     * push JsonObject, which consists of a list of JsonObject of each item.
+     */
+    public Record pushJsonObject(SimTime timestamp, Content content) {
+	Record record = new Record(timestamp, content) ;
+
+	recordList.add(record) ;
+
+	return record ;
+    }
+
+} // class Trail
+
