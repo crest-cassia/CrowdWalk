9acfff4e9c70546601d518a92e6dae4a2af81708
Mitsuo Saito <mitsuo-saitou@aist.go.jp>
2018-12-19 15:30:51 +0900

    GisToCrowdwalk を廃止した。(マップエディタのシェープファイル読み込み機能で代用できるため)

--- GisToCrowdwalk/quickstart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#JAR=./build/libs/GisToCrowdwalk-all.jar
-JAR=`dirname $0`/build/libs/GisToCrowdwalk-all.jar
-
-echo "java -Dfile.encoding=UTF-8 -jar $JAR $*"
-java -Dfile.encoding=UTF-8 -jar $JAR $*
