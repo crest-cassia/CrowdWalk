dee0007e093cc0658b38ae8f9af8ab10b5b5fc85
Shunki Takami <tkmnet@users.noreply.github.com>
2024-01-19 17:26:34 +0900

    Update quickstart.bat to disable HiDPI support func. on Windows

--- crowdwalk/quickstart.bat
+++ crowdwalk/quickstart.bat
@@ -7,7 +7,7 @@ if defined JAVA_HOME (
 set JAVA=%JAVA_HOME%\bin\java
 )
 
-set JAVAOPT=-Dfile.encoding=UTF-8 %JAVA_OPTS%
+set JAVAOPT=-Dfile.encoding=UTF-8 -Dprism.allowhidpi=false %JAVA_OPTS%
 set JAR=%CROWDWALK%\build\libs\crowdwalk.jar
 
 echo %JAVA% %JAVAOPT% -jar %JAR% %*
